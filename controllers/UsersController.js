609fe91703b04a2c85e8bef70c9ae288ad19b605
raxidbou4nja <rachid.boughnja@gmail.com>
2024-06-16 19:34:09 +0100

    feat: Add endpoints for user authentication

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -27,6 +27,26 @@ const UsersController = {
 
         return res.status(201).json(newUser);
     },
+
+    async getMe(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+    
+        const tokenKey = `auth_${token}`;
+        const userId = await redisClient.getAsync(tokenKey);
+        if (!userId) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+    
+        const user = await dbClient.db.collection('users').findOne({ _id: new dbClient.ObjectId(userId) });
+        if (!user) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+    
+        res.status(200).json({ id: user._id, email: user.email });
+    },
 };
 
 export default UsersController;
